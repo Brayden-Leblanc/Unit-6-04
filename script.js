aebbd9d281786b77a2e8cc2bc005de858815e494
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-17 17:08:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = 0
+let b = 0
+let h = 0
+
+document.getElementById('calculate').addEventListener('click', formula)
+
+function formula() {
+  a = document.getElementById('avalue').value
+  b = document.getElementById('bvalue').value
+  h = document.getElementById('hvalue').value
+  
+  a = parseInt(a)
+  b = parseInt(b)
+  h = parseInt(h)
+
+  button = (a + b) / 2 * h
+
+ alert(button) 
+}
