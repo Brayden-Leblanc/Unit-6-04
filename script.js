1337bf74597656e908ea6df8c9d6236fd21725a6
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-17 17:23:54 +0000

    fixed css, html, and js errors

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let a = 0
 let b = 0
 let h = 0
+let button = 0
 
 document.getElementById('calculate').addEventListener('click', formula)
 
-function formula() {
+ function formula() {
   a = document.getElementById('avalue').value
   b = document.getElementById('bvalue').value
   h = document.getElementById('hvalue').value
-  
+
   a = parseInt(a)
   b = parseInt(b)
   h = parseInt(h)
 
   button = (a + b) / 2 * h
 
- alert(button) 
+  alert(button)
 }
